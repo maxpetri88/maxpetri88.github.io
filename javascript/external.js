8c0f303ee41795207fad90ccf9bda4959054f5b5
maxpetri88 <max.e.petri@gmail.com>
2019-09-30 21:02:08 -0500

    Filled content from webmaps array into the webmaps table.

--- javascript/external.js
+++ javascript/external.js
@@ -38,6 +38,18 @@ function condition()
   }
 }
 */
+var webmaps =
+[
+  ["The Historical Marker Database",
+  "https://www.hmdb.org/",
+  "The purpose is to educate the public about local, regional, and global history that occurred based on the historical markers placed across the globe.The user experience is very basic. It looks as though it was created 15 years ago under web 1.0, the homepage shows latest information, topics of interest, and top 10 visited markers. The landing page is nice, but I would prefer an immediate map localized on your location with nearby markers."
+  ],
+  ["Coastal Resource Management Viewer",
+  "https://cgis.glo.texas.gov/rmc/index.html",
+  "The purpose is to aid in development or mineral exploration activities of areas along the Texas coast, certain areas can be developed or not, based on rights of way, dredging, and/or ecological sensitivity. The GIS functionality can query locations by search bar or point and click. Users can also query specific tracts based on Resource Management Code (RMC) that is representative of i.e. Senstive Areas, DA – No Dredging, or OH – Drilling in Deep Water; searching by waterbody and tract; or drawing an area. The authors use ESRI and it shows much more customizability and smooth UX. The authors allow drawing, measuring, printing, creating a report, or turning individual RMC layers on and off."
+  ]
+];
+
 function welcome()
 {
   let a = "Please enter you name.";
@@ -54,12 +66,12 @@ function welcome()
 function webmap_table()
 {
   document.write("<table width=100%>");
-  for (var row=0; row < 2; row++)
+  for (var row=0; row < webmaps.length; row++)
   {
     document.write("<tr>");
-    for (var column=0; column < 3; column++)
+    for (var column=0; column < webmaps[0].length; column++)
     {
-        document.write("<td>" + row + "," + column + "</td>");
+        document.write("<td>" + webmaps[row][column] + "</td>");
     }
     document.write("</tr>");
   }
